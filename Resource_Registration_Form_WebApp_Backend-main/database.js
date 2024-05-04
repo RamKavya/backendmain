f53571cf3ffb74dc603613c8845e629c39ae10b0
RamKavya <139676335+RamKavya@users.noreply.github.com>
2024-05-04 12:33:15 +0530

    Update database.js

--- Resource_Registration_Form_WebApp_Backend-main/database.js
+++ Resource_Registration_Form_WebApp_Backend-main/database.js
@@ -11,8 +11,21 @@ unn = process.env.DB_USERNAMEE;//local
 pwdd = process.env.DB_PASSWORDD;
 
 
-const sequelize = new Sequlize("db_aa5e01_ragu", un, pwd, {
-     host: 'mysql9001.site4now.net',
+// const sequelize = new Sequlize("db_aa5e01_ragu", un, pwd, {
+//      host: 'mysql9001.site4now.net',
+//    // host: 'localhost',
+//     dialect: "mysql",
+//     logging: false,
+//     timezone: '+05:30',
+//     dialectOptions: {
+//         useUTC: false //for reading from database
+//     },
+
+// }) 
+// module.exports = sequelize
+
+const sequelize = new Sequlize("db_aa8549_new001", un, pwd, {
+     host: 'mysql8010.site4now.net',
    // host: 'localhost',
     dialect: "mysql",
     logging: false,
